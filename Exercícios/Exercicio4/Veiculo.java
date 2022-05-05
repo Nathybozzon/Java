0c26291b82c9c236ea1045eb46cdbdc6c10fe39d
Nathybozzon <86155080+Nathybozzon@users.noreply.github.com>
2022-05-05 19:52:11 -0300

    Add files via upload

---
+++ Exercícios/Exercicio4/Veiculo.java
@@ -0,0 +1,15 @@
+public class Veiculo {
+	public String Fabricante;
+	public String Modelo;
+	
+	public String getFab() {
+		//System.out.println(Fabricante);
+		return Fabricante;
+	}
+	public String getMod() {
+		//System.out.println(Modelo);
+		return Modelo;
+	}
+	
+
+}
